0d223de329cf34c8b040f8aedf4d00a3b87593b6
kakastopassolirocha <kaka.stopassoli@gmail.com>
2026-03-10 09:40:26 -0300

    limpa códigos no geral.js

--- dist/js/geral.js
+++ dist/js/geral.js
@@ -1,7 +1,5 @@
 // Aguarda o DOM estar carregado para garantir que os elementos estao disponiveis
 document.addEventListener('DOMContentLoaded', function () {
-    var body = document.body;
-    
     // INICIA LOTTIE DO WHATSAPP
     var whatsappContainer = document.getElementById('whatsapp-fixed');
 
@@ -19,20 +17,4 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
-
-    console.log(backvars.dist + 'lottie/whatsapp2.json');
-
-    window.scrollTo(0, 0);
-    body.classList.add('topScroll');
-
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            body.classList.add('inScroll');
-            body.classList.remove('topScroll');
-            return;
-        }
-
-        body.classList.remove('inScroll');
-        body.classList.add('topScroll');
-    });
 });
